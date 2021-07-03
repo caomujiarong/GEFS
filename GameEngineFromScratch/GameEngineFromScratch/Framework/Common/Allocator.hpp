c89967529aab7be3de4439f5f65f86d0cd36b14b
Jacky Li <caomujiarong@hotmail.com>
2021-07-03 01:33:31 -0700

    complete allocator

--- GameEngineFromScratch/GameEngineFromScratch/Framework/Common/Allocator.hpp
+++ GameEngineFromScratch/GameEngineFromScratch/Framework/Common/Allocator.hpp
@@ -19,6 +19,24 @@ namespace WS {
     
     class Allocator
     {
+    public:
+        // debug patterns
+        static const uint8_t PATTERN_ALIGN = 0xFC;
+        static const uint8_t PATTERN_ALLOC = 0xFD;
+        static const uint8_t PATTERN_FREE = 0xFE;
+
+        Allocator();
+        Allocator(size_t data_size, size_t page_size, size_t alignment);
+        ~Allocator();
+
+        // resets the allocator to a new configuration
+        void Reset(size_t data_size, size_t page_size, size_t alignment);
+
+        // alloc and free blocks
+        void* Allocate();
+        void Free(void* p);
+        void FreeAll();
+
     private:
 #if defined(_DEBUG)
         // fill a free page with debug patterns
@@ -39,22 +57,19 @@ namespace WS {
         // the free block list
         BlockHeader* m_pFreeList;
 
-        size_t m_szDataSize;
-        size_t m_szPageSize;
-        size_t m_szAlignmentSize;
-        size_t m_szBlockSize;
-        size_t m_nBlockPerPage;
+        size_t      m_szDataSize;
+        size_t      m_szPageSize;
+        size_t      m_szAlignmentSize;
+        size_t      m_szBlockSize;
+        uint32_t    m_nBlocksPerPage;
 
         // statistics
-        uint32_t m_nPages;
-        uint32_t m_nBlocks;
-        uint32_t m_nFreeBlocks;
+        uint32_t    m_nPages;
+        uint32_t    m_nBlocks;
+        uint32_t    m_nFreeBlocks;
 
         // disable copy & assignment
-        Allocator(const Allocator& clone);
-        Allocator &operator=(const Allocator &rhs);
-    public:
-        Allocator(/* args */);
-        ~Allocator();
+        Allocator(const Allocator& clone) = delete;
+        Allocator &operator=(const Allocator &rhs) = delete;
     };    
 }
